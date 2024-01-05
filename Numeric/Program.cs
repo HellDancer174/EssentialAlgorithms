cb2ab8dbb43a9a5928260e3d30a17275e994d32c
Виктор Игнатьев <viktorignatev873@gmail.com>
2024-01-05 15:59:07 +0500

    RandomNumber is implemented

---
+++ Numeric/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Numeric;
+
+Console.WriteLine("Hello, World!");
+PrintNext(() => new RandomNumber());
+
+
+
+static void PrintNext<T>(Func<T> valueFunctor)
+{
+    Console.WriteLine(valueFunctor());
+    var key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Escape) return;
+    PrintNext(valueFunctor);
+}
---
+++ Numeric/RandomNumber.cs
@@ -0,0 +1,29 @@
+﻿namespace Numeric;
+
+/// <summary>
+/// Линейный конгруэнтный генератор
+/// </summary>
+public struct RandomNumber
+{
+    private static int n = 0;
+    private static int a = 7;
+    private static int b = 5;
+    private static int m = 11;
+
+
+    private readonly int value;
+
+    public RandomNumber()
+    {
+        var c = (a * n + b);
+        value = c % m;
+        n = value;
+    }
+
+    public int Value => value;
+
+    public override string ToString()
+    {
+        return $"RandomNumber - {Value}";
+    }
+}
